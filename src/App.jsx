2a95b0e6f300ab63eef5304fde1c2c8bda4338cc
sath-wick <sathwik.ponna161@gmail.com>
2025-05-31 22:11:19 +0530

    Fix import case sensitivity for SideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import Background from "../components/Background";
-import Header from "../components/Header";
-import EventRequestsUI from "../components/EventRequests";
-import Sidebar from "../components/SideBar";
 import MainContent from '../components/own/MainContent'
-import MainHeader from "../components/own/MainHeader";
+
 export default function App(){
   return (
     <>
